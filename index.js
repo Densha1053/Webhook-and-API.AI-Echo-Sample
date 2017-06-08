986e237b4e50740b8c38850ce495ae4e1b3e4bd4
Densha1053 <a2p2i2t1@icloud.com>
2017-06-09 00:20:19 +0700

    Create index.js

--- index.js
+++ index.js
@@ -73,6 +73,55 @@ restService.post('/slack-test', function(req, res) {
         }
     });
 });
+'use strict'
+
+var mqtt = require('mqtt')
+
+var clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8)
+
+var host = 'km1.io'
+
+var options = {
+  keepalive: 10,
+  clientId: clientId,
+  protocolId: 'MQTT',
+  protocolVersion: 4,
+  clean: true,
+  reconnectPeriod: 1000,
+  connectTimeout: 30 * 1000,
+  will: {
+    topic: '/Benz1053/room2',
+    payload: req.body.result.parameters.echoText,
+    qos: 0,
+    retain: false
+  },
+  username: 'Benz1053',
+  password: 'benz1053',
+  rejectUnauthorized: false
+}
+
+var client = mqtt.connect(host, options)
+
+client.on('error', function (err) {
+  console.log(err)
+  client.end()
+})
+
+client.on('connect', function () {
+  console.log('client connected:' + clientId)
+})
+
+client.subscribe('topic', { qos: 0 })
+
+client.publish('topic', req.body.result.parameters.echoText, { qos: 0, retain: false })
+
+client.on('message', function (topic, message, packet) {
+  console.log('Received Message:= ' + message.toString() + '\nOn topic:= ' + topic)
+})
+
+client.on('close', function () {
+  console.log(clientId + ' disconnected')
+})
 
 
 
