388b1caa832611580140928d77217199ebd4f451
Densha1053 <a2p2i2t1@icloud.com>
2017-06-09 02:21:17 +0700

    Create index.js

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ restService.post('/echo', function(req, res) {
         speech: speech,
         displayText: speech,
         source: 'webhook-echo-sample'
+        client.publish('topic', speech, { qos: 0, retain: false })
     });
 });
 
