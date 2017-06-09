cc4b6d5b3fc8ba3a7f29479f5bf6b99a222fc3b0
Densha1053 <a2p2i2t1@icloud.com>
2017-06-09 14:04:09 +0700

    Create index.js

--- index.js
+++ index.js
@@ -5,6 +5,55 @@ const bodyParser = require('body-parser');
 
 const restService = express();
 
+var mqtt = require('mqtt')
+
+var clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8)
+
+var host = 'www.km1.io'
+
+var options = {
+  keepalive: 10,
+  clientId: clientId,
+  protocolId: 'MQTT',
+  protocolVersion: 4,
+  clean: true,
+  reconnectPeriod: 1000,
+  connectTimeout: 30 * 1000,
+  will: {
+    topic: '/Benz1053/room2',
+    payload: speech,
+    qos: 0,
+    retain: false
+  },
+  username: 'Benz1053',
+  password: 'benz1053',
+  rejectUnauthorized: false
+}
+
+var client = mqtt.connect(host, options)
+
+client.on('error', function (err) {
+  console.log(err)
+  client.end()
+})
+
+client.on('connect', function () {
+  console.log('client connected:' + clientId)
+})
+
+client.subscribe('topic', { qos: 0 })
+
+client.publish('topic', "speech", { qos: 0, retain: false })
+
+client.on('message', function (topic, message, packet) {
+  console.log('Received Message:= ' + message.toString() + '\nOn topic:= ' + topic)
+})
+
+client.on('close', function () {
+  console.log(clientId + ' disconnected')
+})
+
+
 restService.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -13,6 +62,9 @@ restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    client.on('connect', function () {
+        console.log('client connected:' + clientId)
+    })
     client.publish('topic', speech, { qos: 0, retain: false })
     return res.json({
         speech: speech,
@@ -76,55 +128,6 @@ restService.post('/slack-test', function(req, res) {
         }
     });
 });
-'use strict'
-
-var mqtt = require('mqtt')
-
-var clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8)
-
-var host = 'www.km1.io'
-
-var options = {
-  keepalive: 10,
-  clientId: clientId,
-  protocolId: 'MQTT',
-  protocolVersion: 4,
-  clean: true,
-  reconnectPeriod: 1000,
-  connectTimeout: 30 * 1000,
-  will: {
-    topic: '/Benz1053/room2',
-    payload: speech,
-    qos: 0,
-    retain: false
-  },
-  username: 'Benz1053',
-  password: 'benz1053',
-  rejectUnauthorized: false
-}
-
-var client = mqtt.connect(host, options)
-
-client.on('error', function (err) {
-  console.log(err)
-  client.end()
-})
-
-client.on('connect', function () {
-  console.log('client connected:' + clientId)
-})
-
-client.subscribe('topic', { qos: 0 })
-
-client.publish('topic', "speech", { qos: 0, retain: false })
-
-client.on('message', function (topic, message, packet) {
-  console.log('Received Message:= ' + message.toString() + '\nOn topic:= ' + topic)
-})
-
-client.on('close', function () {
-  console.log(clientId + ' disconnected')
-})
 
 
 
